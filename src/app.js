92a8fb8cd0dbd27c7a762238b7f21506824ae5a0
Leticia Monteiro <leticiamonteiro.design@gmail.com>
2020-07-29 00:16:20 -0300

    feat(rest): init

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
-const express = require("express");
-const cors = require("cors");
+const express = require('express');
+const cors = require('cors');
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require('uuidv4');
 
 const app = express();
 
@@ -10,24 +10,56 @@ app.use(cors());
 
 const repositories = [];
 
-app.get("/repositories", (request, response) => {
-  // TODO
+app.get('/repositories', (req, res) => {
+	return res.status(200).json(repositories);
 });
 
-app.post("/repositories", (request, response) => {
-  // TODO
+app.post('/repositories', (req, res) => {
+	const { title, url, techs } = req.body;
+
+	const repositorie = {
+		id: uuid(),
+		title,
+		url,
+		techs,
+		likes: 0,
+	};
+
+	repositories.push(repositorie);
+
+	return res.status(200).json(repositorie);
 });
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
+app.put('/repositories/:id', (req, res) => {
+	const { id } = req.params;
+
+	const checkRepositorie = repositories.find(
+		(repositorie) => id === repositorie.id
+	);
+
+	if (!checkRepositorie) {
+		return res
+			.status(400)
+			.json({ error: true, message: 'The repository doesnt exist' });
+	}
+
+	return res.status(200).json(checkRepositorie);
 });
 
-app.delete("/repositories/:id", (request, response) => {
-  // TODO
+app.delete('/repositories/:id', (req, res) => {
+	const { id } = req.params;
+
+	const checkRepositorie = repositories.find(
+		(repositorie) => id === repositorie.id
+	);
+
+	if (!checkRepositorie) {
+		return res
+			.status(400)
+			.json({ error: true, message: 'The repository doesnt exist' });
+	}
 });
 
-app.post("/repositories/:id/like", (request, response) => {
-  // TODO
-});
+app.post('/repositories/:id/like', (req, res) => {});
 
 module.exports = app;
